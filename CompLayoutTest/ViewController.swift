2203dcc39817d4d3197e4ce5d53a7fa77ee3d80b
Danil Smenov <semenoff.da2018@yandex.ru>
2020-07-03 10:41:26 +0300

    Initial

--- CompLayoutTest/ViewController.swift
+++ CompLayoutTest/ViewController.swift
@@ -10,11 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
-
 }
 
